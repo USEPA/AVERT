4353d68ca968557d8af2c0d879545944cb3f82f9
Bryan Neva <bryan.neva@erg.com>
2016-08-29 15:06:56 -0400

    Add standard redux folder structure. Swap out App container component with Calculations so I have somwhere to put things

---
+++ react/src/app/Calculations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculations extends Component {
+  render() {
+    return (
+      <div className="Calculations">
+        Calculating...
+      </div>
+    );
+  }
+}
+
+export default Calculations;
--- react/src/index.js
+++ react/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calculations from './app/Calculations';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Calculations />,
   document.getElementById('root')
 );
