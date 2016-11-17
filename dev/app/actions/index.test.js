6d2d140b59d406d1821a1a8bd71cb7dca45972e9
Courtney Myers <courtney.myers@erg.com>
2016-11-17 10:47:03 -0500

    Update npm test script (remove jsdom environment flag, add verbose flag). Add npm coverage script to test test coverage. Format action tests for better legibility (break expect() param onto seperate line). Add tests for Redux reducers.

--- dev/app/actions/index.test.js
+++ dev/app/actions/index.test.js
@@ -1,3 +1,4 @@
+// actions
 import {
   CHANGE_ACTIVE_STEP,
   CHANGE_SELECTED_REGION,
@@ -8,23 +9,29 @@ import {
 describe('actions', () => {
   describe('setActiveStep', () => {
     it('should have a type of CHANGE_ACTIVE_STEP', () => {
-      expect(setActiveStep().type).toEqual(CHANGE_ACTIVE_STEP);
+      expect(
+        setActiveStep().type
+      ).toEqual(CHANGE_ACTIVE_STEP);
     });
 
     it('should pass on the step number we pass in', () => {
-      const number = 2;
-      expect(setActiveStep(number).payload.stepNumber).toEqual(number);
+      expect(
+        setActiveStep(2).payload.stepNumber
+      ).toEqual(2);
     });
   });
 
   describe('changeSelectedRegion', () => {
     it('should have a type of CHANGE_SELECTED_REGION', () => {
-      expect(changeSelectedRegion().type).toEqual(CHANGE_SELECTED_REGION);
+      expect(
+        changeSelectedRegion().type
+      ).toEqual(CHANGE_SELECTED_REGION);
     });
 
     it('should pass on the region name we pass in', () => {
-      const name = 'California';
-      expect(changeSelectedRegion(name).payload.regionName).toEqual(name);
+      expect(
+        changeSelectedRegion('California').payload.regionName
+      ).toEqual('California');
     });
   });
 });
