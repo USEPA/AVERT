279385f89c12df73ed2e4e91b3e47634722030b1
Courtney Myers <courtney.myers@erg.com>
2016-11-17 18:51:24 -0500

    Add unit tests for 'change selected aggregation' and 'change selected unit' actions, and 'selected aggregation' and 'selected unit' reducers.

--- dev/app/actions/index.test.js
+++ dev/app/actions/index.test.js
@@ -2,8 +2,12 @@
 import {
   CHANGE_ACTIVE_STEP,
   CHANGE_SELECTED_REGION,
+  CHANGE_SELECTED_AGGREGATION,
+  CHANGE_SELECTED_UNIT,
   setActiveStep,
-  changeSelectedRegion
+  changeSelectedRegion,
+  changeSelectedAggregation,
+  changeSelectedUnit
 } from '../actions';
 
 describe('actions', () => {
@@ -32,6 +36,34 @@ describe('actions', () => {
       expect(
         changeSelectedRegion('California').payload.regionName
       ).toEqual('California');
+    });
+  });
+
+  describe('changeSelectedAggregation', () => {
+    it('should have a type of CHANGE_SELECTED_AGGREGATION', () => {
+      expect(
+        changeSelectedAggregation().type
+      ).toEqual(CHANGE_SELECTED_AGGREGATION);
+    });
+
+    it('should pass on the aggregate level we pass in', () => {
+      expect(
+        changeSelectedAggregation('State').payload.aggregateLevel
+      ).toEqual('State');
+    });
+  });
+
+  describe('changeSelectedUnit', () => {
+    it('should have a type of CHANGE_SELECTED_UNIT', () => {
+      expect(
+        changeSelectedUnit().type
+      ).toEqual(CHANGE_SELECTED_UNIT);
+    });
+
+    it('should pass on the unit type we pass in', () => {
+      expect(
+        changeSelectedUnit('Percent change').payload.unitType
+      ).toEqual('Percent change');
     });
   });
 });
