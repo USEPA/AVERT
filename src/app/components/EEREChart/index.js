223d9a51332d0d168a245f85bc192791275b1001
Courtney Myers <courtney.myers@erg.com>
2016-11-29 16:32:47 -0500

    Update chart components to use stateless functional component syntax (as 'refs' aren't needed after all on Highcharts components)

--- src/app/components/EEREChart/index.js
+++ src/app/components/EEREChart/index.js
@@ -1,61 +1,59 @@
-import React , { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import Highcharts from 'react-highcharts';
 // styles
 import './styles.css';
 
-class EEREChart extends Component {
-  render() {
-    const data = this.props.hourlyEere.map((hour) => hour.final_mw);
-    const hours = this.props.hourlyEere.map((hour, index) => index);
-    const chartConfig = {
-      chart: {
-        height: 300,
-        style: {
-          fontFamily: '"Open Sans", sans-serif',
-        },
+const EEREChart = (props) => {
+  const data = props.hourlyEere.map((hour) => hour.final_mw);
+  const hours = props.hourlyEere.map((hour, index) => index);
+  const chartConfig = {
+    chart: {
+      height: 300,
+      style: {
+        fontFamily: '"Open Sans", sans-serif',
       },
+    },
+    title: {
+      text: props.heading,
+      style: {
+        fontSize: '1rem',
+        fontWeight: 'bold',
+        color: '#444',
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    legend: {
+      enabled: false,
+    },
+    xAxis: {
+      categories: hours,
+    },
+    yAxis: {
       title: {
-        text: this.props.heading,
-        style: {
-          fontSize: '1rem',
-          fontWeight: 'bold',
-          color: '#444',
-        },
+        text: false,
       },
-      credits: {
-        enabled: false,
-      },
-      legend: {
-        enabled: false,
-      },
-      xAxis: {
-        categories: hours,
-      },
-      yAxis: {
-        title: {
-          text: false,
-        },
-      },
-      series: [{
-        name: 'EERE Load Output',
-        data: data,
-        color: '#058dc7',
-      }],
-    };
-
-    // conditionally define chart if hourlyEere prop exists
-    let chart = null;
-    if (this.props.hourlyEere.length > 0) {
-      chart = (
-        <div className='avert-eere-profile'>
-          <Highcharts config={chartConfig} ref='eere-profile' />
-        </div>
-      );
-    }
-
-    return chart;
+    },
+    series: [{
+      name: 'EERE Load Output',
+      data: data,
+      color: '#058dc7',
+    }],
   };
-}
+
+  // conditionally define chart if hourlyEere prop exists
+  let chart = null;
+  if (props.hourlyEere.length > 0) {
+    chart = (
+      <div className='avert-eere-profile'>
+        <Highcharts config={chartConfig} />
+      </div>
+    );
+  }
+
+  return chart;
+};
 
 EEREChart.propTypes = {
   heading: PropTypes.string.isRequired,
