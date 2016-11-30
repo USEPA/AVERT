d5ecc8a49df0bd032827436e9aa2175685e62870
Courtney Myers <courtney.myers@erg.com>
2016-11-30 10:37:06 -0500

    Update region number/ids to match RDFs

--- src/app/components/RegionList/index.js
+++ src/app/components/RegionList/index.js
@@ -11,15 +11,15 @@ const RegionList = (props) => (
     <option value='' disabled>Select Region</option>
 
     <option value={'1'}>California</option>
-    <option value={'2'}>Upper Midwest</option>
+    <option value={'10'}>Upper Midwest</option>
     <option value={'3'}>Northeast</option>
     <option value={'4'}>Northwest</option>
     <option value={'5'}>Rocky Mountains</option>
-    <option value={'6'}>Great Lakes / Mid-Atlantic</option>
+    <option value={'2'}>Great Lakes / Mid-Atlantic</option>
     <option value={'7'}>Southeast</option>
     <option value={'8'}>Southwest</option>
     <option value={'9'}>Texas</option>
-    <option value={'10'}>Lower Midwest</option>
+    <option value={'6'}>Lower Midwest</option>
   </select>
 );
 
