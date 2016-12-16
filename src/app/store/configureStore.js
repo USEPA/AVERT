d1f3e1cf5cfd775bca916516c1f34ad40d892ed3
Courtney Myers <courtney.myers@erg.com>
2016-12-16 08:29:43 -0500

    Comment out createLogger import, as it's not used in production

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import createLogger from 'redux-logger';
+//import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 // reducers
 import reducers from '../reducers';
