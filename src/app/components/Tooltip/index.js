391125fee3505fd51a80c12e4088b2e59ea0ef69
Courtney Myers <courtney.myers@erg.com>
2017-01-03 18:12:02 -0500

    Change Tooltip component to be a stateless functional component, as 'refs' are no longer needed

--- src/app/components/Tooltip/index.js
+++ src/app/components/Tooltip/index.js
@@ -1,41 +1,39 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 // styles
 import './styles.css';
 
-class Tooltip extends Component {
-  render() {
-    let activeState = 'false';
-    if (this.props.activeModalId === 0) { activeState = 'closed' }
-    if (this.props.activeModalId === this.props.id) { activeState = 'true' }
+const Tooltip = (props) => {
+  let activeState = 'false';
+  if (props.activeModalId === 0) { activeState = 'closed' }
+  if (props.activeModalId === props.id) { activeState = 'true' }
 
-    return (
-      <span>
-        <a href='' className='avert-modal-link'
+  return (
+    <span>
+      <a href='' className='avert-modal-link'
+        onClick={(e) => {
+          e.preventDefault();
+          props.onStoreActiveModal(props.id);
+          props.onToggleModalOverlay();
+        }}
+      >info</a>
+
+      <span className='avert-modal'
+        data-modal-id={ props.id }
+        data-active={ activeState }
+      >
+        <a href='' className='avert-modal-close'
           onClick={(e) => {
             e.preventDefault();
-            this.props.onStoreActiveModal(this.props.id);
-            this.props.onToggleModalOverlay();
+            props.onResetActiveModal();
+            props.onToggleModalOverlay();
           }}
-        >info</a>
+        >×</a>
 
-        <span className='avert-modal'
-          data-modal-id={ this.props.id }
-          data-active={ activeState }
-        >
-          <a href='' className='avert-modal-close'
-            onClick={(e) => {
-              e.preventDefault();
-              this.props.onResetActiveModal();
-              this.props.onToggleModalOverlay();
-            }}
-          >×</a>
-
-          { this.props.children }
-        </span>
+        { props.children }
       </span>
-    );
-  };
-}
+    </span>
+  );
+};
 
 Tooltip.propTypes = {
   children: PropTypes.node,
