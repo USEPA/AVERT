b7a4cf7417ecf43732c7906c340495001b95634e
Courtney Myers <courtney.myers@erg.com>
2017-10-02 10:22:03 -0400

    Format module files. Remove no longer used lodash npm package

--- epa-avert-microservice/app/modules/MonthlyEmissions.js
+++ epa-avert-microservice/app/modules/MonthlyEmissions.js
@@ -1,75 +1,72 @@
 'use strict';
 
-const MonthlyEmissions = (function () {
-    function MonthlyEmissions () {
+module.exports = (function () {
+  function MonthlyEmissions () {
+    const locationLevels = {
+      region: {},
+      state: {},
+      county: {},
+    };
 
-        const locationLevels = {
-            region: {},
-            state: {},
-            county: {},
-        };
+    const standardStructure = {
+      emissions: locationLevels,
+      percentages: locationLevels,
+    };
 
-        const standardStructure = {
-            emissions: locationLevels,
-            percentages: locationLevels,
-        };
+    this.generation = standardStructure;
+    this.so2 = standardStructure;
+    this.nox = standardStructure;
+    this.co2 = standardStructure;
 
-        this.generation = standardStructure;
-        this.so2 = standardStructure;
-        this.nox = standardStructure;
-        this.co2 = standardStructure;
+    this.count = 0;
+  }
 
-        this.count = 0;
+  MonthlyEmissions.prototype.toJSON = function() {
+    return {
+      generation: this.generation,
+      so2: this.so2,
+      nox: this.nox,
+      co2: this.co2,
+      count: this.count,
+    };
+  };
+
+  MonthlyEmissions.prototype.addRegion = function (type, emissionsOrPercentages, month, data) {
+    if (typeof this[type][emissionsOrPercentages].region[month] === 'undefined') {
+      this[type][emissionsOrPercentages].region[month] = 0
     }
 
-    MonthlyEmissions.prototype.toJSON = function() {
-        return {
-            generation: this.generation,
-            so2: this.so2,
-            nox: this.nox,
-            co2: this.co2,
-            count: this.count,
-        };
-    };
+    this[type][emissionsOrPercentages].region[month] += data;
+  };
 
-    MonthlyEmissions.prototype.addRegion = function (type, emissionsOrPercentages, month, data) {
-        if (typeof this[type][emissionsOrPercentages].region[month] === 'undefined') {
-            this[type][emissionsOrPercentages].region[month] = 0
-        }
+  MonthlyEmissions.prototype.addState = function (type, emissionsOrPercentages, state, month, data) {
+    this.count ++;
+    if (typeof this[type][emissionsOrPercentages].state[state] === 'undefined') {
+      this[type][emissionsOrPercentages].state[state] = {}
+    }
 
-        this[type][emissionsOrPercentages].region[month] += data;
-    };
+    if (typeof this[type][emissionsOrPercentages].state[state][month] === 'undefined') {
+      this[type][emissionsOrPercentages].state[state][month] = 0;
+    }
 
-    MonthlyEmissions.prototype.addState = function (type, emissionsOrPercentages, state, month, data) {
-        this.count ++;
-        if (typeof this[type][emissionsOrPercentages].state[state] === 'undefined') {
-            this[type][emissionsOrPercentages].state[state] = {}
-        }
+    this[type][emissionsOrPercentages].state[state][month] += data;
+  };
 
-        if (typeof this[type][emissionsOrPercentages].state[state][month] === 'undefined') {
-            this[type][emissionsOrPercentages].state[state][month] = 0;
-        }
+  MonthlyEmissions.prototype.addCounty = function (type, emissionsOrPercentages, state, county, month, data) {
+    if (typeof this[type][emissionsOrPercentages].county[state] === 'undefined') {
+      this[type][emissionsOrPercentages].county[state] = {}
+    }
 
-        this[type][emissionsOrPercentages].state[state][month] += data;
-    };
+    if (typeof this[type][emissionsOrPercentages].county[state][county] === 'undefined') {
+      this[type][emissionsOrPercentages].county[state][county] = {}
+    }
 
-    MonthlyEmissions.prototype.addCounty = function (type, emissionsOrPercentages, state, county, month, data) {
-        if (typeof this[type][emissionsOrPercentages].county[state] === 'undefined') {
-            this[type][emissionsOrPercentages].county[state] = {}
-        }
+    if (typeof this[type][emissionsOrPercentages].county[state][county][month] === 'undefined') {
+      this[type][emissionsOrPercentages].county[state][county][month] = 0;
+    }
 
-        if (typeof this[type][emissionsOrPercentages].county[state][county] === 'undefined') {
-            this[type][emissionsOrPercentages].county[state][county] = {}
-        }
+    this[type][emissionsOrPercentages].county[state][county][month] += data;
+  };
 
-        if (typeof this[type][emissionsOrPercentages].county[state][county][month] === 'undefined') {
-            this[type][emissionsOrPercentages].county[state][county][month] = 0;
-        }
-
-        this[type][emissionsOrPercentages].county[state][county][month] += data;
-    };
-
-    return MonthlyEmissions;
+  return MonthlyEmissions;
 })();
-
-module.exports = MonthlyEmissions;
--- epa-avert-microservice/app/modules/StateEmissions.js
+++ epa-avert-microservice/app/modules/StateEmissions.js
@@ -1,48 +1,43 @@
 'use strict';
 
-const _ = require('lodash');
+module.exports = (function () {
+  function StateEmissions () {
+    this.generation = {};
+    this.so2 = {};
+    this.nox = {};
+    this.co2 = {};
+  }
 
-const StateEmissions = (function () {
-    function StateEmissions () {
-        this.generation = {};
-        this.so2 = {};
-        this.nox = {};
-        this.co2 = {};
+  StateEmissions.prototype.toJSON = function () {
+    return {
+      generation: this.generation,
+      so2: this.so2,
+      nox: this.nox,
+      co2: this.co2
     }
+  };
 
-    StateEmissions.prototype.toJSON = function () {
-        return {
-            generation: this.generation,
-            so2: this.so2,
-            nox: this.nox,
-            co2: this.co2
-        }
+  StateEmissions.prototype.add = function (type, state, data) {
+    this[type][state] = this[type][state] ? this[type][state] : 0;
+    this[type][state] += data;
+  };
+
+  StateEmissions.prototype.export = function () {
+    const states = Object.keys(this.generation);
+    const data = states.map(function (state) {
+      return {
+        state: state,
+        so2: this.so2,
+        nox: this.nox,
+        co2: this.co2,
+      }
+    });
+
+    return {
+      states: states,
+      data: data,
     };
+  };
 
-    StateEmissions.prototype.add = function (type, state, data) {
-        this[type][state] = this[type][state] ? this[type][state] : 0;
-        this[type][state] += data;
-    };
-
-    StateEmissions.prototype.export = function () {
-        // const states = Object.keys(_.merge(this.generation,this.so2,this.nox,this.co2));
-        const states = Object.keys(this.generation);
-        const data = states.map(function (state) {
-            return {
-                state: state,
-                so2: this.so2,
-                nox: this.nox,
-                co2: this.co2,
-            }
-        });
-
-        return {
-            states: states,
-            data: data,
-        };
-    };
-
-    return StateEmissions;
+  return StateEmissions;
 })();
-
-module.exports = StateEmissions;
