a902291df6acf23b4047b3c0774b5cb75fcfd3f6
Courtney Myers <courtney.myers@erg.com>
2017-11-24 18:17:07 -0500

    Improve readibility of MonthlyEmissions module: TODO: use consistent implementation for undefined checking in all webservice modules classes (some use typeof checking, some use ternary assignment)

--- epa-avert-webservice/app/modules/MonthlyEmissions.js
+++ epa-avert-webservice/app/modules/MonthlyEmissions.js
@@ -26,40 +26,32 @@ module.exports = (function () {
   }
 
   MonthlyEmissions.prototype.addRegion = function (type, emissionsOrPercentages, month, data) {
-    if (typeof this[type][emissionsOrPercentages].region[month] === 'undefined') {
-      this[type][emissionsOrPercentages].region[month] = 0
-    }
+    let value = this[type][emissionsOrPercentages].region[month];
 
-    this[type][emissionsOrPercentages].region[month] += data;
+    if (typeof value === 'undefined') value = 0;
+
+    value += data;
   };
 
   MonthlyEmissions.prototype.addState = function (type, emissionsOrPercentages, state, month, data) {
     this.count ++;
-    if (typeof this[type][emissionsOrPercentages].state[state] === 'undefined') {
-      this[type][emissionsOrPercentages].state[state] = {}
-    }
 
-    if (typeof this[type][emissionsOrPercentages].state[state][month] === 'undefined') {
-      this[type][emissionsOrPercentages].state[state][month] = 0;
-    }
+    let value = this[type][emissionsOrPercentages].state[state];
 
-    this[type][emissionsOrPercentages].state[state][month] += data;
+    if (typeof value === 'undefined') value = {};
+    if (typeof value[month] === 'undefined') value[month] = 0;
+
+    value[month] += data;
   };
 
   MonthlyEmissions.prototype.addCounty = function (type, emissionsOrPercentages, state, county, month, data) {
-    if (typeof this[type][emissionsOrPercentages].county[state] === 'undefined') {
-      this[type][emissionsOrPercentages].county[state] = {}
-    }
+    let value = this[type][emissionsOrPercentages].county[state];
 
-    if (typeof this[type][emissionsOrPercentages].county[state][county] === 'undefined') {
-      this[type][emissionsOrPercentages].county[state][county] = {}
-    }
+    if (typeof value === 'undefined') value = {};
+    if (typeof value[county] === 'undefined') value[county] = {};
+    if (typeof value[county][month] === 'undefined') value[county][month] = 0;
 
-    if (typeof this[type][emissionsOrPercentages].county[state][county][month] === 'undefined') {
-      this[type][emissionsOrPercentages].county[state][county][month] = 0;
-    }
-
-    this[type][emissionsOrPercentages].county[state][county][month] += data;
+    value[county][month] += data;
   };
 
   return MonthlyEmissions;
