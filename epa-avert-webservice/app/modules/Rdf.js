5c6b8270d9a9f73377a8db03a2460e305f13909a
Courtney Myers <courtney.myers@erg.com>
2017-11-28 18:27:13 -0500

    Simplify Rdf module in web service

--- epa-avert-webservice/app/modules/Rdf.js
+++ epa-avert-webservice/app/modules/Rdf.js
@@ -2,46 +2,43 @@
 
 module.exports = (function () {
   function Rdf (options) {
+    this.defaults = {};
     this.raw = {};
     this.regionName = '';
     this.regionalLoads = [];
-    this.percentLimit = [];
-    this.defaults = {};
     this.months = [];
     this.edges = [];
 
-    if (options && typeof options.rdf !== 'undefined') this.setRdf(options.rdf);
-    if (options && typeof options.defaults !== 'undefined') this.setDefaults(options.defaults);
+    if (options && typeof options.defaults !== 'undefined') {
+      this.defaults = options.defaults;
+    }
+
+    if (options && typeof options.rdf !== 'undefined') {
+      this.raw = options.rdf;
+      this.regionName = options.rdf.region.region_name;
+
+      options.rdf.regional_load.forEach(function (item) {
+        this.regionalLoads.push(item.regional_load_mw);
+        this.months.push(item.month);
+      }, this);
+
+      // --- TODO: ensure next version of data files display "load_bin_edges"
+      // as an array, so Object.keys map() can be removed ---
+      this.edges = Object.keys(options.rdf.load_bin_edges).map(function (key) {
+        return options.rdf.load_bin_edges[key]
+      });
+    }
   }
 
   Rdf.prototype.toJSON = function () {
     return {
+      defaults: this.defaults,
       raw: this.raw,
       regionName: this.regionName,
       regionalLoads: this.regionalLoads,
-      percentLimit: this.percentLimit,
-      defaults: this.defaults,
       months: this.months,
       edges: this.edges,
     }
-  };
-
-  Rdf.prototype.setRdf = function (rdf) {
-    this.raw = rdf;
-    this.regionName = rdf.region.region_name;
-    this.regionalLoads = rdf.regional_load.map(function (data) {
-      return data.regional_load_mw
-    });
-    this.edges = Object.keys(rdf.load_bin_edges).map(function (key) {
-      return rdf.load_bin_edges[key]
-    });
-    this.months = rdf.regional_load.map(function (item) {
-      return item.month
-    });
-  };
-
-  Rdf.prototype.setDefaults = function (defaults) {
-    this.defaults = defaults;
   };
 
   return Rdf;
