254668ad9258e4ea8e625a46b5d7fdbae4d937e6
Courtney Myers <courtney.myers@erg.com>
2017-11-30 09:20:30 -0500

    Set X-Frame-Options header to allow from EPA's domain

--- epa-avert-webservice/app/config/middleware.js
+++ epa-avert-webservice/app/config/middleware.js
@@ -1,12 +1,14 @@
 const logger = require('koa-logger');
 const cors = require('koa-cors');
 const json = require('koa-json');
+const setHeaders = require('../lib/setHeaders');
 const pageNotFound = require('../lib/pageNotFound');
 
 const middleware = [
   logger(),
   cors(),
   json(),
+  setHeaders,
   pageNotFound,
 ];
 
