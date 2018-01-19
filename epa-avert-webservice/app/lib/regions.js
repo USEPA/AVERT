00addfbfc59770e67eb031aff6d1138f62a01672
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-01-19 17:57:22 -0500

    Add 2016 rdfs to webservice data directory. Move 2015 rdfs to a new '2015' subdirectory. Update webservice's lib/regions file to point to new 2016 rdf files.

--- epa-avert-webservice/app/lib/regions.js
+++ epa-avert-webservice/app/lib/regions.js
@@ -2,51 +2,51 @@ module.exports = {
   CA: {
     label: 'California',
     defaults: 'app/data/eere-defaults-california.json',
-    rdf: 'app/data/rdf_california_2015.json',
+    rdf: 'app/data/rdf_CA_2016.json',
   },
   EMW: {
     label: 'Great Lakes / Mid-Atlantic',
     defaults: 'app/data/eere-defaults-great-lakes-mid-atlantic.json',
-    rdf: 'app/data/rdf_great-lakes-mid-atlantic_2015.json',
+    rdf: 'app/data/rdf_EMW_2016.json',
   },
   NE: {
     label: 'Northeast',
     defaults: 'app/data/eere-defaults-northeast.json',
-    rdf: 'app/data/rdf_northeast_2015.json',
+    rdf: 'app/data/rdf_NE_2016.json',
   },
   NW: {
     label: 'Northwest',
     defaults: 'app/data/eere-defaults-northwest.json',
-    rdf: 'app/data/rdf_northwest_2015.json',
+    rdf: 'app/data/rdf_NW_2016.json',
   },
   RM: {
     label: 'Rocky Mountains',
     defaults: 'app/data/eere-defaults-rocky-mountains.json',
-    rdf: 'app/data/rdf_rocky-mountains_2015.json',
+    rdf: 'app/data/rdf_RM_2016.json',
   },
   SC: {
     label: 'Lower Midwest',
     defaults: 'app/data/eere-defaults-lower-midwest.json',
-    rdf: 'app/data/rdf_lower-midwest_2015.json',
+    rdf: 'app/data/rdf_SC_2016.json',
   },
   SE: {
     label: 'Southeast',
     defaults: 'app/data/eere-defaults-southeast.json',
-    rdf: 'app/data/rdf_southeast_2015.json',
+    rdf: 'app/data/rdf_SE_2016.json',
   },
   SW: {
     label: 'Southwest',
     defaults: 'app/data/eere-defaults-southwest.json',
-    rdf: 'app/data/rdf_southwest_2015.json',
+    rdf: 'app/data/rdf_AZNM_2016.json',
   },
   TX: {
     label: 'Texas',
     defaults: 'app/data/eere-defaults-texas.json',
-    rdf: 'app/data/rdf_texas_2015.json',
+    rdf: 'app/data/rdf_TX_2016.json',
   },
   WMW: {
     label: 'Upper Midwest',
     defaults: 'app/data/eere-defaults-upper-midwest.json',
-    rdf: 'app/data/rdf_upper-midwest_2015.json',
+    rdf: 'app/data/rdf_WMW_2016.json',
   },
 };
