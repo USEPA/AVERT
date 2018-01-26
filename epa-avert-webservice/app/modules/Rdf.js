ae0b4f6b63ffe7e521fa84c3953d3b515edf5099
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-01-26 17:00:06 -0500

    Update webservice Rdf module: Data files have been normalized, so setting Rdf.edges is much more straightforward

--- epa-avert-webservice/app/modules/Rdf.js
+++ epa-avert-webservice/app/modules/Rdf.js
@@ -2,42 +2,39 @@
 
 module.exports = (function () {
   function Rdf (options) {
-    this.defaults = {};
     this.raw = {};
     this.regionName = '';
+    this.edges = [];
     this.regionalLoads = [];
     this.months = [];
-    this.edges = [];
+    this.defaults = {};
 
-    if (options && typeof options.defaults !== 'undefined') {
-      this.defaults = options.defaults;
-    }
-
+    // if called with options.rdf
     if (options && typeof options.rdf !== 'undefined') {
       this.raw = options.rdf;
       this.regionName = options.rdf.region.region_name;
+      this.edges = options.rdf.load_bin_edges;
 
       options.rdf.regional_load.forEach(function (item) {
         this.regionalLoads.push(item.regional_load_mw);
         this.months.push(item.month);
       }, this);
+    }
 
-      // --- TODO: ensure next version of data files display "load_bin_edges"
-      // as an array, so Object.keys map() can be removed ---
-      this.edges = Object.keys(options.rdf.load_bin_edges).map(function (key) {
-        return options.rdf.load_bin_edges[key]
-      });
+    // if called with options.defaults
+    if (options && typeof options.defaults !== 'undefined') {
+      this.defaults = options.defaults;
     }
   }
 
   Rdf.prototype.toJSON = function () {
     return {
-      defaults: this.defaults,
       raw: this.raw,
       regionName: this.regionName,
+      edges: this.edges,
       regionalLoads: this.regionalLoads,
       months: this.months,
-      edges: this.edges,
+      defaults: this.defaults,
     }
   };
 
