9ea3c5859a1f490b6cd3896a69e2da28d8fea105
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-02-09 14:40:29 -0500

    Add flow types and organize Region container in RegionMap component

---
+++ epa-avert-webapp/src/app/components/RegionMap/components/Region/container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+// components
+import Region from './index.js';
+// actions
+import { selectRegion } from 'app/actions';
+
+const mapStateToProps = (state) => ({
+  selectedRegion: state.regions.region,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRegionClick(regionId) {
+    dispatch(selectRegion(regionId));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Region);
