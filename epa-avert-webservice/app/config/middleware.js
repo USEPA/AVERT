0ff474169cff3b4760941cabc09bfcba1c501f0d
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-02-21 15:35:24 -0500

    Replace third-party koa-cors with official @koa/cors package. Update koa-logger package to latest version. Remove unnecessary koa-json package.

--- epa-avert-webservice/app/config/middleware.js
+++ epa-avert-webservice/app/config/middleware.js
@@ -1,6 +1,5 @@
 const logger = require('koa-logger');
-const cors = require('koa-cors');
-const json = require('koa-json');
+const cors = require('@koa/cors');
 const serve = require('koa-static');
 
 const setHeaders = require('../lib/setHeaders');
@@ -10,7 +9,6 @@ const middleware = [
   setHeaders,
   logger(),
   cors(),
-  json(),
   serve('./app/public'),
   pageNotFound,
 ];
