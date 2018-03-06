9efd7e9c2019ec52f5cf4d6ae93a8939be2124c4
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-03-06 11:38:56 -0500

    Add koa-basic-auth package and conditionally include it as middleware if AUTH_USER env variable is set

---
+++ epa-avert-webservice/app/lib/basicAuth.js
@@ -0,0 +1,13 @@
+module.exports = async function(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.set('WWW-Authenticate', 'Basic');
+      ctx.body = 'Authentication Required';
+    } else {
+      throw err;
+    }
+  }
+}
