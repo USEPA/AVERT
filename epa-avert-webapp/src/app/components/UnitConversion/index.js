077cb75d110ac8e0482dce4cb8a0fde8fa9c435c
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-04-18 09:48:32 -0400

    Update UnitConversion component: Re-order inputs, update label above inputs, and add Tooltips to kW and GW inputs

--- epa-avert-webapp/src/app/components/UnitConversion/index.js
+++ epa-avert-webapp/src/app/components/UnitConversion/index.js
@@ -1,6 +1,8 @@
 // @flow
 
 import React from 'react';
+// components
+import Tooltip from 'app/components/Tooltip/container.js';
 // styles
 import './styles.css';
 
@@ -65,16 +67,34 @@ class UnitConversion extends React.Component<Props, State> {
   render() {
     return (
       <div className="avert-unit-conversion">
-        <p>Unit conversions:</p>
+        <p>Helpful Unit conversions:</p>
 
         <div className="avert-unit-fields">
           <div className="avert-unit-field">
             <input
               type="text"
-              value={this.state.gw}
-              onChange={(e) => this.updateInputs(e.target.value, 'gw')}
+              value={this.state.kw}
+              onChange={(e) => this.updateInputs(e.target.value, 'kw')}
             />
-            <span> GW = </span>
+            <span className="avert-unit-field-text"> kW</span>
+
+            <Tooltip id={8}>
+              To convert <strong>kWh</strong> to <strong>kW</strong>, divide kWh
+              by the <em>total number of hours in the year</em>
+              <sup>*</sup>.
+              <br />
+              <br />
+              <sup>*</sup>Non-leap years: <strong>8760</strong> hours
+              <br />
+              <sup>*</sup>Leap years: <strong>8784</strong> hours
+              <br />
+              <br />
+              <strong>2016 (leap-year) example:</strong>
+              <br />
+              8784 kWh ÷ 8784 hrs = 1 kW
+            </Tooltip>
+
+            <span className="avert-unit-field-text">= </span>
           </div>
 
           <div className="avert-unit-field">
@@ -83,16 +103,32 @@ class UnitConversion extends React.Component<Props, State> {
               value={this.state.mw}
               onChange={(e) => this.updateInputs(e.target.value, 'mw')}
             />
-            <span> MW = </span>
+            <span className="avert-unit-field-text"> MW = </span>
           </div>
 
           <div className="avert-unit-field">
             <input
               type="text"
-              value={this.state.kw}
-              onChange={(e) => this.updateInputs(e.target.value, 'kw')}
+              value={this.state.gw}
+              onChange={(e) => this.updateInputs(e.target.value, 'gw')}
             />
-            <span> kW</span>
+            <span className="avert-unit-field-text"> GW</span>
+
+            <Tooltip id={9}>
+              To convert <strong>GW</strong> to <strong>GWh</strong>, multiply
+              GW by the <em>total number of hours in the year</em>
+              <sup>*</sup>.
+              <br />
+              <br />
+              <sup>*</sup>Non-leap years: <strong>8760</strong> hours
+              <br />
+              <sup>*</sup>Leap years: <strong>8784</strong> hours
+              <br />
+              <br />
+              <strong>2016 (leap-year) example:</strong>
+              <br />
+              1 GW × 8784 hrs = 8784 GWh
+            </Tooltip>
           </div>
         </div>
 
