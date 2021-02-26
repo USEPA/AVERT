313c949366fa4424422f17539a27f14e49290569
Courtney Myers <courtney.myers@erg.com>
2021-02-26 10:31:10 -0500

    Replace service worker with web-vitals in app template (per latest create react app release)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import 'react-app-polyfill/stable';
 import 'core-js/features/array/includes';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 // components
 import App from 'app/components/App';
 // store
@@ -18,7 +18,7 @@ ReactDOM.render(
   rootElement,
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
