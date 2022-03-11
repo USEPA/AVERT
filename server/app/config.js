adf43964b7c81f11d662ff4fafb27ca7aa7bfd17
Courtney Myers <courtney.myers@erg.com>
2022-03-10 23:30:41 -0500

    Add 2021 RDFs and latest annual emissions factors

--- server/app/config.js
+++ server/app/config.js
@@ -1,61 +1,61 @@
 module.exports = {
-  year: 2019,
+  year: 2021,
   regions: {
     CA: {
       eere: 'eere-defaults-california.json',
-      rdf: 'rdf_CA_2019.json',
+      rdf: 'rdf_CA_2021.json',
     },
     CENT: {
       eere: 'eere-defaults-central.json',
-      rdf: 'rdf_CENT_2019.json',
+      rdf: 'rdf_CENT_2021.json',
     },
     FL: {
       eere: 'eere-defaults-florida.json',
-      rdf: 'rdf_FL_2019.json',
+      rdf: 'rdf_FL_2021.json',
     },
     MIDA: {
       eere: 'eere-defaults-midatlantic.json',
-      rdf: 'rdf_MIDA_2019.json',
+      rdf: 'rdf_MIDA_2021.json',
     },
     MIDW: {
       eere: 'eere-defaults-midwest.json',
-      rdf: 'rdf_MIDW_2019.json',
+      rdf: 'rdf_MIDW_2021.json',
     },
     NCSC: {
       eere: 'eere-defaults-carolinas.json',
-      rdf: 'rdf_NCSC_2019.json',
+      rdf: 'rdf_NCSC_2021.json',
     },
     NE: {
       eere: 'eere-defaults-newengland.json',
-      rdf: 'rdf_NE_2019.json',
+      rdf: 'rdf_NE_2021.json',
     },
     NW: {
       eere: 'eere-defaults-northwest.json',
-      rdf: 'rdf_NW_2019.json',
+      rdf: 'rdf_NW_2021.json',
     },
     NY: {
       eere: 'eere-defaults-newyork.json',
-      rdf: 'rdf_NY_2019.json',
+      rdf: 'rdf_NY_2021.json',
     },
     RM: {
       eere: 'eere-defaults-rockymountains.json',
-      rdf: 'rdf_RM_2019.json',
+      rdf: 'rdf_RM_2021.json',
     },
     SE: {
       eere: 'eere-defaults-southeast.json',
-      rdf: 'rdf_SE_2019.json',
+      rdf: 'rdf_SE_2021.json',
     },
     SW: {
       eere: 'eere-defaults-southwest.json',
-      rdf: 'rdf_SW_2019.json',
+      rdf: 'rdf_SW_2021.json',
     },
     TE: {
       eere: 'eere-defaults-texas.json',
-      rdf: 'rdf_TE_2019.json',
+      rdf: 'rdf_TE_2021.json',
     },
     TN: {
       eere: 'eere-defaults-tennessee.json',
-      rdf: 'rdf_TN_2019.json',
+      rdf: 'rdf_TN_2021.json',
     },
   }
 };
