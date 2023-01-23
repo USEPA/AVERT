e4aedede965ae33bb517829d5d3f189e873cf258
Courtney Myers <courtney.myers@erg.com>
2023-01-23 17:22:07 -0500

    Update downloads reducer's formatting of county download data to properly account for it a pollutant needs replacement, and the formatting of counties with 'city' in their name

--- client/src/app/redux/reducers/downloads.ts
+++ client/src/app/redux/reducers/downloads.ts
@@ -5,6 +5,7 @@ import type { RegionId } from 'app/config';
 import { regions as regionsConfig } from 'app/config';
 
 type EmissionsData = EmissionsChanges[string]['data'];
+type EmissionsFlagsField = EmissionsChanges[string]['emissionsFlags'][number];
 type MonthlyData = EmissionsData[keyof EmissionsData];
 
 type Pollutant = 'SO2' | 'NOX' | 'CO2' | 'PM25' | 'VOCS' | 'NH3';
@@ -15,18 +16,18 @@ type CountyData = {
   State: string | null;
   County: string | null;
   'Unit of measure': 'percent' | 'emissions (tons)' | 'emissions (pounds)';
-  'Power Sector: January': number;
-  'Power Sector: February': number;
-  'Power Sector: March': number;
-  'Power Sector: April': number;
-  'Power Sector: May': number;
-  'Power Sector: June': number;
-  'Power Sector: July': number;
-  'Power Sector: August': number;
-  'Power Sector: September': number;
-  'Power Sector: October': number;
-  'Power Sector: November': number;
-  'Power Sector: December': number;
+  'Power Sector: January': number | null;
+  'Power Sector: February': number | null;
+  'Power Sector: March': number | null;
+  'Power Sector: April': number | null;
+  'Power Sector: May': number | null;
+  'Power Sector: June': number | null;
+  'Power Sector: July': number | null;
+  'Power Sector: August': number | null;
+  'Power Sector: September': number | null;
+  'Power Sector: October': number | null;
+  'Power Sector: November': number | null;
+  'Power Sector: December': number | null;
 };
 
 type CobraData = {
@@ -85,9 +86,13 @@ export default function reducer(
 export function setDownloadData(): AppThunk {
   return (dispatch, getState) => {
     const { results } = getState();
-    const { emissionsMonthlyData } = results;
+    const { emissionsMonthlyData, egusNeedingEmissionsReplacement } = results;
 
-    const countyData = formatCountyDownloadData(emissionsMonthlyData);
+    const countyData = formatCountyDownloadData({
+      emissionsMonthlyData,
+      egusNeedingEmissionsReplacement,
+    });
+
     const cobraData = [] as CountyData[]; // TODO
 
     dispatch({
@@ -104,7 +109,11 @@ export function setDownloadData(): AppThunk {
  * Formats monthly emissions data in a format that supports downloading the
  * county level data as a CSV file.
  */
-function formatCountyDownloadData(emissionsMonthlyData: EmissionsMonthlyData) {
+function formatCountyDownloadData(options: {
+  emissionsMonthlyData: EmissionsMonthlyData;
+  egusNeedingEmissionsReplacement: EmissionsChanges;
+}) {
+  const { emissionsMonthlyData, egusNeedingEmissionsReplacement } = options;
   const { total, regions, states, counties } = emissionsMonthlyData;
 
   const result: CountyData[] = [];
@@ -112,6 +121,8 @@ function formatCountyDownloadData(emissionsMonthlyData: EmissionsMonthlyData) {
   if (Object.keys(total).length === 0) return result;
 
   const pollutantsRows = createOrderedPollutantsRows();
+
+  const egusNeedingReplacement = Object.values(egusNeedingEmissionsReplacement);
 
   /**
    * Conditionally add all affected regions data
@@ -121,8 +132,16 @@ function formatCountyDownloadData(emissionsMonthlyData: EmissionsMonthlyData) {
     const totalRows = [...pollutantsRows].map((row) => {
       const { pollutant, unit } = row;
 
+      const pollutantNeedsReplacement = egusNeedingReplacement.some((egu) => {
+        return egu.emissionsFlags.includes(pollutant as EmissionsFlagsField);
+      });
+
       const monthlyData = (total as EmissionsData)[pollutant];
-      const monthlyFields = createMonthlyFields({ monthlyData, unit });
+      const monthlyFields = createMonthlyFields({
+        pollutantNeedsReplacement,
+        monthlyData,
+        unit,
+      });
 
       return {
         Pollutant: pollutant.toUpperCase() as Pollutant,
@@ -144,8 +163,19 @@ function formatCountyDownloadData(emissionsMonthlyData: EmissionsMonthlyData) {
     const regionsRows = [...pollutantsRows].map((row) => {
       const { pollutant, unit } = row;
 
+      const pollutantNeedsReplacement = egusNeedingReplacement.some((egu) => {
+        return (
+          egu.emissionsFlags.includes(pollutant as EmissionsFlagsField) &&
+          egu.region === regionId
+        );
+      });
+
       const monthlyData = regionData[pollutant];
-      const monthlyFields = createMonthlyFields({ monthlyData, unit });
+      const monthlyFields = createMonthlyFields({
+        pollutantNeedsReplacement,
+        monthlyData,
+        unit,
+      });
 
       return {
         Pollutant: pollutant.toUpperCase() as Pollutant,
@@ -167,8 +197,19 @@ function formatCountyDownloadData(emissionsMonthlyData: EmissionsMonthlyData) {
     const statesRows = [...pollutantsRows].map((row) => {
       const { pollutant, unit } = row;
 
+      const pollutantNeedsReplacement = egusNeedingReplacement.some((egu) => {
+        return (
+          egu.emissionsFlags.includes(pollutant as EmissionsFlagsField) &&
+          egu.state === stateId
+        );
+      });
+
       const monthlyData = stateData[pollutant];
-      const monthlyFields = createMonthlyFields({ monthlyData, unit });
+      const monthlyFields = createMonthlyFields({
+        pollutantNeedsReplacement,
+        monthlyData,
+        unit,
+      });
 
       return {
         Pollutant: pollutant.toUpperCase() as Pollutant,
@@ -191,14 +232,26 @@ function formatCountyDownloadData(emissionsMonthlyData: EmissionsMonthlyData) {
       const countiesRows = [...pollutantsRows].map((row) => {
         const { pollutant, unit } = row;
 
+        const pollutantNeedsReplacement = egusNeedingReplacement.some((egu) => {
+          return (
+            egu.emissionsFlags.includes(pollutant as EmissionsFlagsField) &&
+            egu.state === stateId &&
+            egu.county === countyName
+          );
+        });
+
         const monthlyData = countyData[pollutant];
-        const monthlyFields = createMonthlyFields({ monthlyData, unit });
+        const monthlyFields = createMonthlyFields({
+          pollutantNeedsReplacement,
+          monthlyData,
+          unit,
+        });
 
         return {
           Pollutant: pollutant.toUpperCase() as Pollutant,
           'Aggregation level': 'County',
           State: stateId,
-          County: countyName,
+          County: countyName.replace(/city/, '(City)'), // format 'city'
           'Unit of measure': unit,
           ...monthlyFields,
         };
@@ -239,10 +292,11 @@ function createOrderedPollutantsRows() {
  * Creates monthly fields from monthly data and unit.
  */
 function createMonthlyFields(options: {
+  pollutantNeedsReplacement: boolean;
   monthlyData: MonthlyData;
   unit: 'percent' | 'emissions (tons)' | 'emissions (pounds)';
 }) {
-  const { monthlyData, unit } = options;
+  const { pollutantNeedsReplacement, monthlyData, unit } = options;
 
   const result = Object.entries(monthlyData).reduce((object, [key, data]) => {
     const month = Number(key);
@@ -251,10 +305,15 @@ function createMonthlyFields(options: {
     const emissionsChange = postEere - original;
     const percentChange = (emissionsChange / original) * 100 || 0;
 
-    object[month] = unit === 'percent' ? percentChange : emissionsChange;
+    object[month] =
+      unit === 'percent'
+        ? pollutantNeedsReplacement
+          ? null
+          : percentChange
+        : emissionsChange;
 
     return object;
-  }, {} as { [month: number]: number });
+  }, {} as { [month: number]: number | null });
 
   return {
     'Power Sector: January': result[1],
