53669ef57d4be6192203828cf3afe6413d19a278
Courtney Myers <courtney.myers@erg.com>
2023-01-27 15:09:13 -0500

    Update StateEmissionsTable to set and display annual state emissions changes from power sector emissions monthly data, and already calculated state transportation sector emissions changes

--- client/src/app/components/StateEmissionsTable.tsx
+++ client/src/app/components/StateEmissionsTable.tsx
@@ -4,56 +4,33 @@ import type {
   EmissionsData,
   EmissionsMonthlyData,
 } from 'app/redux/reducers/results';
+import type { VehicleEmissionChangesByGeography } from 'app/calculations/transportation';
 import type { StateId } from 'app/config';
 import { states as statesConfig } from 'app/config';
 
+/**
+ * Round number to the nearest 10 and conditionally format resulting -0 to 0.
+ */
 function formatNumber(number: number) {
-  const output = Math.round(number / 10) * 10;
-  return output.toLocaleString();
+  const result = Math.round(number / 10) * 10;
+  return (result === -0 ? 0 : result).toLocaleString();
 }
 
 /**
- * Sum the emissions monthly data into total annual state changes for each
- * pollutant.
+ * Sum the power sector emissions monthly data into total annual changes for
+ * each pollutant at each state, and returns those state power sector results
+ * with state transportation sector results.
  */
-function setAnnualStateEmissionsChanges(
-  emissionsMonthlyData: EmissionsMonthlyData,
-) {
-  if (!emissionsMonthlyData) return [];
+function setAnnualStateEmissionsChanges(options: {
+  emissionsMonthlyData: EmissionsMonthlyData;
+  vehicleEmissionChangesByGeography: VehicleEmissionChangesByGeography | {};
+}) {
+  const { emissionsMonthlyData, vehicleEmissionChangesByGeography } = options;
 
-  const { states } = emissionsMonthlyData;
-
-  const result = Object.entries(states).reduce(
-    (array, [stateKey, stateData]) => {
-      const stateId = stateKey as StateId;
-
-      const state = {
-        id: stateId,
-        name: statesConfig[stateId].name,
-        generation: 0,
-        so2: 0,
-        nox: 0,
-        co2: 0,
-        pm25: 0,
-        vocs: 0,
-        nh3: 0,
-      };
-
-      Object.entries(stateData).forEach(([key, annualData]) => {
-        const pollutant = key as keyof EmissionsData;
-
-        Object.values(annualData).forEach((monthlyData) => {
-          state[pollutant] += monthlyData.postEere - monthlyData.original;
-        });
-      });
-
-      array.push(state);
-
-      return array;
-    },
-    [] as {
-      id: StateId;
-      name: string;
+  const result = [] as {
+    id: StateId;
+    name: string;
+    power: {
       generation: number;
       so2: number;
       nox: number;
@@ -61,19 +38,89 @@ function setAnnualStateEmissionsChanges(
       pm25: number;
       vocs: number;
       nh3: number;
-    }[],
-  );
+    };
+    transportation: {
+      CO2: number;
+      NOX: number;
+      SO2: number;
+      PM25: number;
+      VOCs: number;
+      NH3: number;
+    };
+  }[];
 
-  return result;
+  const vehicleEmissionChanges =
+    Object.keys(vehicleEmissionChangesByGeography).length !== 0
+      ? (vehicleEmissionChangesByGeography as VehicleEmissionChangesByGeography)
+      : null;
+
+  if (!emissionsMonthlyData || !vehicleEmissionChanges) return [];
+
+  /** Add power sector data */
+  Object.entries(emissionsMonthlyData.states).forEach(([key, stateData]) => {
+    const stateId = key as StateId;
+    const stateName = statesConfig[stateId].name;
+
+    const power = Object.entries(stateData).reduce(
+      (object, [key, annualData]) => {
+        const pollutant = key as keyof EmissionsData;
+
+        Object.values(annualData).forEach((monthlyData) => {
+          const { original, postEere } = monthlyData;
+          object[pollutant] = postEere - original;
+        });
+
+        return object;
+      },
+      { generation: 0, so2: 0, nox: 0, co2: 0, pm25: 0, vocs: 0, nh3: 0 },
+    );
+
+    if (stateName) {
+      result.push({
+        id: stateId,
+        name: stateName,
+        power,
+        transportation: { CO2: 0, NOX: 0, SO2: 0, PM25: 0, VOCs: 0, NH3: 0 },
+      });
+    }
+  });
+
+  /** Add transportation sector data */
+  Object.entries(vehicleEmissionChanges.states).forEach(([key, stateData]) => {
+    const stateId = key as StateId;
+    const stateName = statesConfig[stateId].name;
+
+    const existingState = result.find((state) => state.id === stateId);
+
+    if (existingState) {
+      existingState.transportation = stateData;
+    }
+
+    if (!existingState && stateName) {
+      result.push({
+        id: stateId,
+        name: stateName,
+        power: { generation: 0, so2: 0, nox: 0, co2: 0, pm25: 0, vocs: 0, nh3: 0 }, // prettier-ignore
+        transportation: stateData,
+      });
+    }
+  });
+
+  return result.sort((a, b) => a.name.localeCompare(b.name));
 }
 
 export function StateEmissionsTable() {
   const emissionsMonthlyData = useTypedSelector(
     ({ results }) => results.emissionsMonthlyData,
   );
+  const vehicleEmissionChangesByGeography = useTypedSelector(
+    ({ transportation }) => transportation.vehicleEmissionChangesByGeography,
+  );
 
-  const annualStateEmissionsChanges =
-    setAnnualStateEmissionsChanges(emissionsMonthlyData);
+  const annualStateEmissionsChanges = setAnnualStateEmissionsChanges({
+    emissionsMonthlyData,
+    vehicleEmissionChangesByGeography,
+  });
 
   if (!emissionsMonthlyData) return null;
 
@@ -106,74 +153,84 @@ export function StateEmissionsTable() {
           </thead>
 
           <tbody>
-            {annualStateEmissionsChanges
-              .sort((stateA, stateB) => stateA.name.localeCompare(stateB.name))
-              .map((stateData, index, array) => {
-                /** apply zebra striping to groups of state data rows */
-                const stripedRowClassName =
-                  index % 2 ? '' : 'avert-table-striped-row';
+            {annualStateEmissionsChanges.map((data, index, array) => {
+              /** apply zebra striping to groups of state data rows */
+              const stripedRowClassName =
+                index % 2 ? '' : 'avert-table-striped-row';
 
-                /** add visual space below the last state row, unless its the last state */
-                const stateSpacingClassName =
-                  index !== array.length - 1
-                    ? 'border-width-1 border-top-width-0 border-x-width-0 border-solid'
-                    : '';
+              /** add visual space below the last state row, unless its the last state */
+              const stateSpacingClassName =
+                index !== array.length - 1
+                  ? 'border-width-1 border-top-width-0 border-x-width-0 border-solid'
+                  : '';
 
-                return (
-                  <Fragment key={stateData.id}>
-                    <tr className={stripedRowClassName}>
-                      <td rowSpan={3}>{stateData.name}</td>
-                      <td className="width-1px text-no-wrap text-right">
-                        <small>From</small> Fossil Generation
-                      </td>
-                      <td className="font-mono-xs text-right">
-                        {formatNumber(stateData.so2)}
-                      </td>
-                      <td className="font-mono-xs text-right">
-                        {formatNumber(stateData.nox)}
-                      </td>
-                      <td className="font-mono-xs text-right">
-                        {formatNumber(stateData.co2)}
-                      </td>
-                      <td className="font-mono-xs text-right">
-                        {formatNumber(stateData.pm25)}
-                      </td>
-                      <td className="font-mono-xs text-right">
-                        {formatNumber(stateData.vocs)}
-                      </td>
-                      <td className="font-mono-xs text-right">
-                        {formatNumber(stateData.nh3)}
-                      </td>
-                    </tr>
+              return (
+                <Fragment key={data.id}>
+                  <tr className={stripedRowClassName}>
+                    <td rowSpan={3}>{data.name}</td>
+                    <td className="width-1px text-no-wrap text-right">
+                      <small>From</small> Fossil Generation
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.power.so2)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.power.nox)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.power.co2)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.power.pm25)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.power.vocs)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.power.nh3)}
+                    </td>
+                  </tr>
 
-                    <tr className={stripedRowClassName}>
-                      <td className="width-1px text-no-wrap text-right">
-                        <small>From</small> Vehicles
-                      </td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                    </tr>
+                  <tr className={stripedRowClassName}>
+                    <td className="width-1px text-no-wrap text-right">
+                      <small>From</small> Vehicles
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.transportation.SO2)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.transportation.NOX)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.transportation.CO2)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.transportation.PM25)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.transportation.VOCs)}
+                    </td>
+                    <td className="font-mono-xs text-right">
+                      {formatNumber(data.transportation.NH3)}
+                    </td>
+                  </tr>
 
-                    <tr
-                      className={`${stripedRowClassName} ${stateSpacingClassName}`}
-                    >
-                      <td className="width-1px text-no-wrap text-right">
-                        Net Change
-                      </td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                      <td className="font-mono-xs text-right">{'---'}</td>
-                    </tr>
-                  </Fragment>
-                );
-              })}
+                  <tr
+                    className={`${stripedRowClassName} ${stateSpacingClassName}`}
+                  >
+                    <td className="width-1px text-no-wrap text-right">
+                      Net Change
+                    </td>
+                    <td className="font-mono-xs text-right">{'---'}</td>
+                    <td className="font-mono-xs text-right">{'---'}</td>
+                    <td className="font-mono-xs text-right">{'---'}</td>
+                    <td className="font-mono-xs text-right">{'---'}</td>
+                    <td className="font-mono-xs text-right">{'---'}</td>
+                    <td className="font-mono-xs text-right">{'---'}</td>
+                  </tr>
+                </Fragment>
+              );
+            })}
           </tbody>
         </table>
       </div>
