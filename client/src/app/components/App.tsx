8eb4bc15e6c96ac7fb6b8df1b69db364805b9811
Courtney Myers <courtney.myers@erg.com>
2023-01-30 08:20:16 -0500

    Add ErrorBoundary with default message, and include it inside App component (wrapping everything but the container div)

--- client/src/app/components/App.tsx
+++ client/src/app/components/App.tsx
@@ -1,14 +1,17 @@
 import '@uswds/uswds';
 import '@uswds/uswds/css/uswds.css';
 // ---
+import { ErrorBoundary } from 'app/components/ErrorBoundary';
 import { PanelTabs } from 'app/components/PanelTabs';
 import { Panels } from 'app/components/Panels';
 
 export function App() {
   return (
     <div className="avert-container">
-      <PanelTabs />
-      <Panels />
+      <ErrorBoundary>
+        <PanelTabs />
+        <Panels />
+      </ErrorBoundary>
     </div>
   );
 }
