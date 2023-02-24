5d50c2b59d3e39912fd00d6a83eb284e6c2e4572
Courtney Myers <courtney.myers@erg.com>
2023-02-24 13:27:20 -0500

    Update StateEmissionsTable to render a multi-select (with no states initially selected) that controls states displayed in the table

--- client/src/app/components/StateEmissionsTable.tsx
+++ client/src/app/components/StateEmissionsTable.tsx
@@ -1,4 +1,5 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import Select from 'react-select';
 // ---
 import { ErrorBoundary } from 'app/components/ErrorBoundary';
 import { useTypedSelector } from 'app/redux/index';
@@ -81,131 +82,164 @@ function StateEmissionsTableContent() {
     combinedSectorsEmissionsData,
   );
 
+  const [selectedStates, setSelectedStates] = useState<
+    typeof annualStateEmissionsChanges
+  >([]);
+
   if (!combinedSectorsEmissionsData) return null;
 
   return (
-    <div className="overflow-auto">
-      <div className="avert-table-container">
-        <table className="avert-table width-full">
-          <thead>
-            <tr>
-              <th colSpan={2}>State</th>
-              <th className="text-right">
-                SO<sub>2</sub> <small>(lb)</small>
-              </th>
-              <th className="text-right">
-                NO<sub>X</sub> <small>(lb)</small>
-              </th>
-              <th className="text-right">
-                CO<sub>2</sub> <small>(tons)</small>
-              </th>
-              <th className="text-right">
-                PM<sub>2.5</sub> <small>(lb)</small>
-              </th>
-              <th className="text-right">
-                VOCs <small>(lb)</small>
-              </th>
-              <th className="text-right">
-                NH<sub>3</sub> <small>(lb)</small>
-              </th>
-            </tr>
-          </thead>
+    <>
+      <Select
+        className="margin-bottom-2"
+        placeholder="Select States"
+        isMulti={true}
+        options={annualStateEmissionsChanges}
+        getOptionValue={(option) => option.id}
+        getOptionLabel={(option) => option.name}
+        onChange={(options) => {
+          const states = Array.isArray(options)
+            ? (options as typeof annualStateEmissionsChanges)
+            : [];
+          setSelectedStates(states);
+        }}
+      />
 
-          <tbody>
-            {annualStateEmissionsChanges.map((data, index, array) => {
-              /** apply zebra striping to groups of state data rows */
-              const stripedRowClassName =
-                index % 2 ? '' : 'avert-table-striped-row';
+      {selectedStates.length === 0 ? (
+        <div className="grid-col-12 margin-bottom-2">
+          <div className="avert-box padding-3">
+            <p className="margin-0 font-sans-xs text-center">
+              <strong>No state selected.</strong>
+              <br />
+              Please select a state.
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="overflow-auto">
+          <div className="avert-table-container">
+            <table className="avert-table width-full">
+              <thead>
+                <tr>
+                  <th colSpan={2}>State</th>
+                  <th className="text-right">
+                    SO<sub>2</sub> <small>(lb)</small>
+                  </th>
+                  <th className="text-right">
+                    NO<sub>X</sub> <small>(lb)</small>
+                  </th>
+                  <th className="text-right">
+                    CO<sub>2</sub> <small>(tons)</small>
+                  </th>
+                  <th className="text-right">
+                    PM<sub>2.5</sub> <small>(lb)</small>
+                  </th>
+                  <th className="text-right">
+                    VOCs <small>(lb)</small>
+                  </th>
+                  <th className="text-right">
+                    NH<sub>3</sub> <small>(lb)</small>
+                  </th>
+                </tr>
+              </thead>
 
-              /** add visual space below the last state row, unless its the last state */
-              const stateSpacingClassName =
-                index !== array.length - 1
-                  ? 'border-width-1 border-top-width-0 border-x-width-0 border-solid border-white'
-                  : '';
+              <tbody>
+                {selectedStates.map((data, index, array) => {
+                  /** apply zebra striping to groups of state data rows */
+                  const stripedRowClassName =
+                    index % 2 ? '' : 'avert-table-striped-row';
 
-              return (
-                <Fragment key={data.id}>
-                  <tr className={stripedRowClassName}>
-                    <td rowSpan={3}>{data.name}</td>
-                    <td className="width-1px text-no-wrap text-right">
-                      <small>From</small> Fossil Generation
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.so2)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.nox)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.co2)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.pm25)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.vocs)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.nh3)}
-                    </td>
-                  </tr>
+                  /** add visual space below the last state row, unless its the last state */
+                  const stateSpacingClassName =
+                    index !== array.length - 1
+                      ? 'border-width-1 border-top-width-0 border-x-width-0 border-solid border-white'
+                      : '';
 
-                  <tr className={stripedRowClassName}>
-                    <td className="width-1px text-no-wrap text-right">
-                      <small>From</small> Vehicles
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.vehicle.so2)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.vehicle.nox)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.vehicle.co2)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.vehicle.pm25)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.vehicle.vocs)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.vehicle.nh3)}
-                    </td>
-                  </tr>
+                  return (
+                    <Fragment key={data.id}>
+                      <tr className={stripedRowClassName}>
+                        <td rowSpan={3}>{data.name}</td>
+                        <td className="width-1px text-no-wrap text-right">
+                          <small>From</small> Fossil Generation
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.so2)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.nox)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.co2)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.pm25)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.vocs)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.nh3)}
+                        </td>
+                      </tr>
 
-                  <tr
-                    className={`${stripedRowClassName} ${stateSpacingClassName}`}
-                  >
-                    <td className="width-1px text-no-wrap text-right">
-                      Net Change
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.so2 + data.vehicle.so2)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.nox + data.vehicle.nox)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.co2 + data.vehicle.co2)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.pm25 + data.vehicle.pm25)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.vocs + data.vehicle.vocs)}
-                    </td>
-                    <td className="font-mono-xs text-right">
-                      {formatNumber(data.power.nh3 + data.vehicle.nh3)}
-                    </td>
-                  </tr>
-                </Fragment>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
+                      <tr className={stripedRowClassName}>
+                        <td className="width-1px text-no-wrap text-right">
+                          <small>From</small> Vehicles
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.vehicle.so2)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.vehicle.nox)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.vehicle.co2)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.vehicle.pm25)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.vehicle.vocs)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.vehicle.nh3)}
+                        </td>
+                      </tr>
+
+                      <tr
+                        className={`${stripedRowClassName} ${stateSpacingClassName}`}
+                      >
+                        <td className="width-1px text-no-wrap text-right">
+                          Net Change
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.so2 + data.vehicle.so2)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.nox + data.vehicle.nox)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.co2 + data.vehicle.co2)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.pm25 + data.vehicle.pm25)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.vocs + data.vehicle.vocs)}
+                        </td>
+                        <td className="font-mono-xs text-right">
+                          {formatNumber(data.power.nh3 + data.vehicle.nh3)}
+                        </td>
+                      </tr>
+                    </Fragment>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
