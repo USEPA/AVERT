481ffa1a0eb6241d20a1e5baafd6ebcd38936d9a
Courtney Myers <courtney.myers@erg.com>
2023-04-07 14:30:19 -0400

    Update display of energy impacts inputs shown on results page to take up much less space

--- client/src/app/components/ResultsImpactsInputs.tsx
+++ client/src/app/components/ResultsImpactsInputs.tsx
@@ -1,7 +1,10 @@
 import { ErrorBoundary } from 'app/components/ErrorBoundary';
-import { ImpactsTextInput } from 'app/components/ImpactsTextInput';
-import { ImpactsSelectInput } from 'app/components/ImpactsSelectInput';
 import { useTypedSelector } from 'app/redux/index';
+
+/** Boolean check if provided input value exists or is not zero */
+function inputExists(value: string) {
+  return value !== '' && value !== '0';
+}
 
 function ResultsImpactsInputsContent() {
   const inputs = useTypedSelector(({ impacts }) => impacts.inputs);
@@ -37,276 +40,166 @@ function ResultsImpactsInputsContent() {
     evDeploymentLocationOptions,
   } = selectOptions;
 
-  const eeInputsEmpty =
-    (constantMwh === '' || constantMwh === '0') &&
-    (annualGwh === '' || annualGwh === '0') &&
-    (broadProgram === '' || broadProgram === '0') &&
-    (reduction === '' || reduction === '0') &&
-    (topHours === '' || topHours === '0');
-
-  const reInputsEmpty =
-    (onshoreWind === '' || onshoreWind === '0') &&
-    (offshoreWind === '' || offshoreWind === '0') &&
-    (utilitySolar === '' || utilitySolar === '0') &&
-    (rooftopSolar === '' || rooftopSolar === '0');
-
-  const evInputsEmpty =
-    (batteryEVs === '' || batteryEVs === '0') &&
-    (hybridEVs === '' || hybridEVs === '0') &&
-    (transitBuses === '' || transitBuses === '0') &&
-    (schoolBuses === '' || schoolBuses === '0');
+  const evInputsNotEmpty =
+    inputExists(batteryEVs) ||
+    inputExists(hybridEVs) ||
+    inputExists(transitBuses) ||
+    inputExists(schoolBuses);
 
   if (!combinedSectorsEmissionsData) return null;
 
+  const evDeploymentLocationName = evDeploymentLocationOptions.find((opt) => {
+    return opt.id === evDeploymentLocation;
+  })?.name;
+
+  const evModelYearName = evModelYearOptions.find((opt) => {
+    return opt.id === evModelYear;
+  })?.name;
+
+  const iceReplacementVehicleName = iceReplacementVehicleOptions.find((opt) => {
+    return opt.id === iceReplacementVehicle;
+  })?.name;
+
   return (
-    <div className="font-sans-xs">
-      {!eeInputsEmpty && (
-        <div className="margin-top-105">
-          <p className="avert-box-heading avert-blue font-serif-xs text-bold">
-            Energy Efficiency inputs:
+    <div className="avert-box padding-105">
+      <p className="avert-box-heading font-serif-2xs line-height-serif-2">
+        Energy Impacts Inputs:
+      </p>
+
+      <div className="avert-columns">
+        {inputExists(annualGwh) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">
+              Reduce total annual generation by:
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{annualGwh}</span>{' '}
+            <span className="font-sans-3xs">GWh</span>
           </p>
+        )}
 
-          <div className="margin-top-1">
-            <div className="tablet:display-flex">
-              <div className="flex-1 tablet:margin-right-2">
-                <ImpactsTextInput
-                  label={<>Reduce total annual generation by:</>}
-                  ariaLabel="Number of GWh expected to be saved in a single year"
-                  suffix="GWh"
-                  value={annualGwh}
-                  fieldName="annualGwh"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-
-              <div className="flex-1 tablet:margin-left-2">
-                <ImpactsTextInput
-                  className="margin-top-1 tablet:margin-top-0"
-                  label={<>Reduce hourly generation by:</>}
-                  ariaLabel="Constant reduction for every hour of the year, in MW"
-                  suffix="MW"
-                  value={constantMwh}
-                  fieldName="constantMwh"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
-
-            <div className="tablet:display-flex">
-              <div className="flex-1 tablet:margin-right-2">
-                <ImpactsTextInput
-                  label={
-                    <>
-                      <em>Broad-based program:</em> Reduce generation by:
-                    </>
-                  }
-                  ariaLabel="Load reduction percentage applied to all hours of the year"
-                  suffix="%&nbsp;in&nbsp;all&nbsp;hours"
-                  value={broadProgram}
-                  fieldName="broadProgram"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-
-              <div className="flex-1 tablet:margin-left-2">
-                <ImpactsTextInput
-                  className="margin-top-1 tablet:margin-top-0"
-                  label={
-                    <>
-                      <em>Targeted program:</em> Reduce generation by:
-                    </>
-                  }
-                  ariaLabel="Load reduction (as a fraction of peaking load) that would be targeted"
-                  suffix="%&nbsp;during&nbsp;the&nbsp;peak:&nbsp;&nbsp;"
-                  value={reduction}
-                  fieldName="reduction"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-
-                <ImpactsTextInput
-                  ariaLabel="Fraction of high-demand hours that the program is expected to affect"
-                  suffix="%&nbsp;of&nbsp;hours"
-                  value={topHours}
-                  fieldName="topHours"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {!reInputsEmpty && (
-        <div className="margin-top-105">
-          <p className="avert-box-heading avert-blue font-serif-xs text-bold">
-            Renewable Energy inputs:
+        {inputExists(constantMwh) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Reduce hourly generation by:</span>{' '}
+            <span className="font-mono-xs text-bold">{constantMwh}</span>{' '}
+            <span className="font-sans-3xs">MW</span>
           </p>
+        )}
 
-          <div className="margin-top-1">
-            <div className="tablet:display-flex">
-              <div className="flex-1 tablet:margin-right-2">
-                <ImpactsTextInput
-                  label={<>Onshore wind total capacity:</>}
-                  ariaLabel="Total capacity (maximum potential electricity generation) in MW"
-                  suffix="MW"
-                  value={onshoreWind}
-                  fieldName="onshoreWind"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-
-              <div className="flex-1 tablet:margin-left-2">
-                <ImpactsTextInput
-                  label={<>Offshore wind total capacity:</>}
-                  ariaLabel="Total capacity (maximum potential electricity generation) in MW"
-                  suffix="MW"
-                  value={offshoreWind}
-                  fieldName="offshoreWind"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
-
-            <div className="tablet:display-flex">
-              <div className="flex-1 tablet:margin-right-2">
-                <ImpactsTextInput
-                  label={<>Utility-scale solar PV total capacity:</>}
-                  ariaLabel="Total capacity (maximum potential electricity generation) in MW"
-                  suffix="MW"
-                  value={utilitySolar}
-                  fieldName="utilitySolar"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-
-              <div className="flex-1 tablet:margin-left-2">
-                <ImpactsTextInput
-                  className="margin-top-1 tablet:margin-top-0"
-                  label={<>Distributed (rooftop) solar PV total capacity:</>}
-                  ariaLabel="Total capacity (maximum potential electricity generation) in MW"
-                  suffix="MW"
-                  value={rooftopSolar}
-                  fieldName="rooftopSolar"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {!evInputsEmpty && (
-        <div className="margin-top-105">
-          <p className="avert-box-heading avert-blue font-serif-xs text-bold">
-            Electric Vehicles inputs:
+        {inputExists(broadProgram) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">
+              <em>Broad-based program:</em> Reduce generation by:
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{broadProgram}</span>{' '}
+            <span className="font-sans-3xs">%&nbsp;in&nbsp;all&nbsp;hours</span>
           </p>
+        )}
 
-          <div className="margin-top-1">
-            <div className="tablet:display-flex desktop:margin-right-2">
-              <div className="flex-1 tablet:margin-right-2">
-                <ImpactsTextInput
-                  label={<>Light-duty battery EVs:</>}
-                  ariaLabel="Number of light-duty battery EVs to be added to the road"
-                  value={batteryEVs}
-                  fieldName="batteryEVs"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
+        {(inputExists(reduction) || inputExists(topHours)) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">
+              <em>Targeted program:</em> Reduce generation by:
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{reduction}</span>{' '}
+            <span className="font-sans-3xs">
+              %&nbsp;during&nbsp;the&nbsp;peak:{' '}
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{topHours}</span>{' '}
+            <span className="font-sans-3xs">%&nbsp;of&nbsp;hours</span>
+          </p>
+        )}
 
-              <div className="flex-1 tablet:margin-left-2">
-                <ImpactsTextInput
-                  className="margin-top-1 tablet:margin-top-0"
-                  label={<>Light-duty plug-in hybrid EVs:</>}
-                  ariaLabel="Number of light-duty plug-in hybrid EVs to be added to the road"
-                  value={hybridEVs}
-                  fieldName="hybridEVs"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
+        {inputExists(onshoreWind) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Onshore wind total capacity:</span>{' '}
+            <span className="font-mono-xs text-bold">{onshoreWind}</span>{' '}
+            <span className="font-sans-3xs">MW</span>
+          </p>
+        )}
 
-            <div className="tablet:display-flex desktop:margin-right-2">
-              <div className="flex-1 tablet:margin-right-2">
-                <ImpactsTextInput
-                  className="margin-top-1"
-                  label={<>Electric transit buses:</>}
-                  ariaLabel="Number of electric transit buses to be added to the road"
-                  value={transitBuses}
-                  fieldName="transitBuses"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
+        {inputExists(offshoreWind) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Offshore wind total capacity:</span>{' '}
+            <span className="font-mono-xs text-bold">{offshoreWind}</span>{' '}
+            <span className="font-sans-3xs">MW</span>
+          </p>
+        )}
 
-              <div className="flex-1 tablet:margin-left-2">
-                <ImpactsTextInput
-                  className="margin-top-1"
-                  label={<>Electric school buses:</>}
-                  ariaLabel="Number of electric school buses to be added to the road"
-                  value={schoolBuses}
-                  fieldName="schoolBuses"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
+        {inputExists(utilitySolar) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">
+              Utility-scale solar PV total capacity:
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{utilitySolar}</span>{' '}
+            <span className="font-sans-3xs">MW</span>
+          </p>
+        )}
 
-            <div className="desktop:display-flex">
-              <div className="flex-1 desktop:margin-right-2">
-                <ImpactsSelectInput
-                  className="margin-top-1"
-                  label="Location of EV deployment:"
-                  ariaLabel="Location of EV deployment"
-                  options={evDeploymentLocationOptions}
-                  value={evDeploymentLocation}
-                  fieldName="evDeploymentLocation"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
+        {inputExists(rooftopSolar) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">
+              Distributed (rooftop) solar PV total capacity:
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{rooftopSolar}</span>{' '}
+            <span className="font-sans-3xs">MW</span>
+          </p>
+        )}
 
-              <div className="flex-1 desktop:margin-x-2">
-                <ImpactsSelectInput
-                  className="margin-top-1"
-                  label="EV model year:"
-                  ariaLabel="Model year of the modeled electric vehicles"
-                  options={evModelYearOptions}
-                  value={evModelYear}
-                  fieldName="evModelYear"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
+        {inputExists(batteryEVs) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Light-duty battery EVs:</span>{' '}
+            <span className="font-mono-xs text-bold">{batteryEVs}</span>
+          </p>
+        )}
 
-              <div className="flex-1 desktop:margin-left-2">
-                <ImpactsSelectInput
-                  className="margin-top-1"
-                  label="ICE vehicles being replaced:"
-                  ariaLabel="EV to displace a “new” or the average “existing” internal combustion engine vehicle"
-                  options={iceReplacementVehicleOptions}
-                  value={iceReplacementVehicle}
-                  fieldName="iceReplacementVehicle"
-                  disabled="true"
-                  onChange={(value) => {}}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+        {inputExists(hybridEVs) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">
+              Light-duty plug-in hybrid EVs:
+            </span>{' '}
+            <span className="font-mono-xs text-bold">{hybridEVs}</span>
+          </p>
+        )}
 
-      <hr className="avert-rule" />
+        {inputExists(transitBuses) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Electric transit buses:</span>{' '}
+            <span className="font-mono-xs text-bold">{transitBuses}</span>
+          </p>
+        )}
+
+        {inputExists(schoolBuses) && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Electric school buses:</span>{' '}
+            <span className="font-mono-xs text-bold">{schoolBuses}</span>
+          </p>
+        )}
+
+        {evInputsNotEmpty && evDeploymentLocationName && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">Location of EV deployment:</span>{' '}
+            <span className="font-mono-xs text-bold">
+              {evDeploymentLocationName}
+            </span>
+          </p>
+        )}
+
+        {evInputsNotEmpty && evModelYearName && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">EV model year:</span>{' '}
+            <span className="font-mono-xs text-bold">{evModelYearName}</span>
+          </p>
+        )}
+
+        {evInputsNotEmpty && iceReplacementVehicleName && (
+          <p className="margin-bottom-0 padding-top-1 line-height-sans-2">
+            <span className="font-sans-2xs">ICE vehicles being replaced:</span>{' '}
+            <span className="font-mono-xs text-bold">
+              {iceReplacementVehicleName}
+            </span>
+          </p>
+        )}
+      </div>
     </div>
   );
 }
