64235378684d673b2b129356955a1b7b6c2f22b6
Courtney Myers <courtney.myers@erg.com>
2023-04-10 18:29:57 -0400

    Add tailwind preflight css (to not clash with EPA styles)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 // ---
 import { App } from 'app/components/App';
 import { store } from 'app/redux/store';
+import './tailwind-preflight.css';
 import './styles.css';
 
 const container = document.getElementById('root') as HTMLElement;
