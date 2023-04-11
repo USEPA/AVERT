7886a80cab913b6d2eb03d06ac59a1ae4572334c
Courtney Myers <courtney.myers@erg.com>
2023-04-10 23:35:23 -0400

    Replace 2021 RDFs with 2022 RDFs

--- server/app/config.js
+++ server/app/config.js
@@ -1,61 +1,61 @@
 module.exports = {
-  year: 2021,
+  year: 2022,
   regions: {
     CA: {
-      eere: 'eere-defaults-california.json',
-      rdf: 'rdf_CA_2021.json',
+      eere: "eere-defaults-california.json",
+      rdf: "rdf_CA_2022.json",
     },
     CENT: {
-      eere: 'eere-defaults-central.json',
-      rdf: 'rdf_CENT_2021.json',
+      eere: "eere-defaults-central.json",
+      rdf: "rdf_CENT_2022.json",
     },
     FL: {
-      eere: 'eere-defaults-florida.json',
-      rdf: 'rdf_FL_2021.json',
+      eere: "eere-defaults-florida.json",
+      rdf: "rdf_FL_2022.json",
     },
     MIDA: {
-      eere: 'eere-defaults-midatlantic.json',
-      rdf: 'rdf_MIDA_2021.json',
+      eere: "eere-defaults-midatlantic.json",
+      rdf: "rdf_MIDA_2022.json",
     },
     MIDW: {
-      eere: 'eere-defaults-midwest.json',
-      rdf: 'rdf_MIDW_2021.json',
+      eere: "eere-defaults-midwest.json",
+      rdf: "rdf_MIDW_2022.json",
     },
     NCSC: {
-      eere: 'eere-defaults-carolinas.json',
-      rdf: 'rdf_NCSC_2021.json',
+      eere: "eere-defaults-carolinas.json",
+      rdf: "rdf_NCSC_2022.json",
     },
     NE: {
-      eere: 'eere-defaults-newengland.json',
-      rdf: 'rdf_NE_2021.json',
+      eere: "eere-defaults-newengland.json",
+      rdf: "rdf_NE_2022.json",
     },
     NW: {
-      eere: 'eere-defaults-northwest.json',
-      rdf: 'rdf_NW_2021.json',
+      eere: "eere-defaults-northwest.json",
+      rdf: "rdf_NW_2022.json",
     },
     NY: {
-      eere: 'eere-defaults-newyork.json',
-      rdf: 'rdf_NY_2021.json',
+      eere: "eere-defaults-newyork.json",
+      rdf: "rdf_NY_2022.json",
     },
     RM: {
-      eere: 'eere-defaults-rockymountains.json',
-      rdf: 'rdf_RM_2021.json',
+      eere: "eere-defaults-rockymountains.json",
+      rdf: "rdf_RM_2022.json",
     },
     SE: {
-      eere: 'eere-defaults-southeast.json',
-      rdf: 'rdf_SE_2021.json',
+      eere: "eere-defaults-southeast.json",
+      rdf: "rdf_SE_2022.json",
     },
     SW: {
-      eere: 'eere-defaults-southwest.json',
-      rdf: 'rdf_SW_2021.json',
+      eere: "eere-defaults-southwest.json",
+      rdf: "rdf_SW_2022.json",
     },
     TE: {
-      eere: 'eere-defaults-texas.json',
-      rdf: 'rdf_TE_2021.json',
+      eere: "eere-defaults-texas.json",
+      rdf: "rdf_TE_2022.json",
     },
     TN: {
-      eere: 'eere-defaults-tennessee.json',
-      rdf: 'rdf_TN_2021.json',
+      eere: "eere-defaults-tennessee.json",
+      rdf: "rdf_TN_2022.json",
     },
-  }
+  },
 };
