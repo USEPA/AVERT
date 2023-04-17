5f9a5355061d4a22f932f5ade5c437e9d33bd96f
Courtney Myers <courtney.myers@erg.com>
2023-04-17 17:06:02 -0400

    Build client app for production

--- server/app/public/post-message.js
+++ server/app/public/post-message.js
@@ -1,25 +1,23 @@
-/* prettier-ignore */
 (function () {
-  // get height of current document, and send to parent window
+  // get height of the current document, and send to the parent window
   function sendHeight() {
-    var height = document.querySelector('html').offsetHeight;
+    const height = document.querySelector('body').offsetHeight;
     window.parent.postMessage(['setHeight', height], '*');
   }
 
   // delay sending height, so height is calculated after all elements load
-  document.addEventListener('DOMContentLoaded', function(ev) {
+  document.addEventListener('DOMContentLoaded', (_ev) => {
     window.setTimeout(sendHeight, 1000);
-  }, false);
+  });
 
   // delay sending height, so height is calculated after click
-  document.addEventListener('click', function(ev) {
+  document.addEventListener('click', (_ev) => {
     window.setTimeout(sendHeight, 0);
-  }, false);
+  });
 
   // delegate delayed sending of height to only 'Enter' keypress
-  document.addEventListener('keypress', function(ev) {
-    if ((ev.key && ev.key === 'Enter') || (ev.keyCode && ev.keyCode === 13)) {
-      window.setTimeout(sendHeight, 0);
-    }
-  }, false);
+  document.addEventListener('keydown', (ev) => {
+    if (ev.key !== 'Enter') return;
+    window.setTimeout(sendHeight, 0);
+  });
 })();
