eeadfe0f4ff2c4a15671e2565bc85021f12ed6ac
Courtney Myers <courtney.myers@erg.com>
2023-10-11 14:51:58 -0400

    Update server app's dependencies and update server app to use renamed @koa/bodyparser (previous version was koa-bodyparser)

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
-const Sentry = require('@sentry/node');
-const Koa = require('koa');
-const cors = require('@koa/cors');
-const auth = require('koa-basic-auth');
-const serve = require('koa-static');
-const logger = require('koa-logger');
-const bodyParser = require('koa-bodyparser');
+const Sentry = require("@sentry/node");
+const Koa = require("koa");
+const cors = require("@koa/cors");
+const auth = require("koa-basic-auth");
+const serve = require("koa-static");
+const logger = require("koa-logger");
+const { bodyParser } = require("@koa/bodyparser");
 
-const routes = require('./app/routes');
-const middleware = require('./app/middleware');
+const routes = require("./app/routes");
+const middleware = require("./app/middleware");
 
 const app = new Koa();
 
@@ -21,7 +21,7 @@ Sentry.init({
   autoSessionTracking: false,
 });
 
-app.on('error', (err, ctx) => {
+app.on("error", (err, ctx) => {
   Sentry.withScope((scope) => {
     scope.addEventProcessor((event) => {
       return Sentry.Handlers.parseRequest(event, ctx.request);
@@ -33,27 +33,29 @@ app.on('error', (err, ctx) => {
 // setup initial middleware
 app.use(logger());
 app.use(cors());
-app.use(bodyParser({ jsonLimit: '50mb' }));
+app.use(bodyParser({ jsonLimit: "50mb" }));
 app.use(middleware.customHeaders);
 
 // setup routes
-routes.forEach(route => app.use(route));
+routes.forEach((route) => app.use(route));
 
 // conditionally apply basic auth to downstream middleware
-if (process.env.AVERT_AUTH === 'true') {
+if (process.env.AVERT_AUTH === "true") {
   app.use(middleware.basicAuth);
   app.use(auth({ name: process.env.AVERT_USER, pass: process.env.AVERT_PASS }));
 }
 
 // setup serving of static files and custom 404
-app.use(serve('./app/public'));
+app.use(serve("./app/public"));
 app.use(middleware.pageNotFound);
 
 // configure port and start server
 app.port = process.env.PORT || 3001;
 
-if (!module.parent) app.listen(app.port, () => {
-  console.log(`Koa is running on http://localhost:${app.port}`);
-});
+if (!module.parent) {
+  app.listen(app.port, () => {
+    console.log(`Koa is running on http://localhost:${app.port}`);
+  });
+}
 
 module.exports = app;
