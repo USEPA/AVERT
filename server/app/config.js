ba33c0506349415660612715bd6a6c85d2ca0ecb
Courtney Myers <courtney.myers@erg.com>
2024-02-13 21:07:28 -0500

    Add storage defaults for California region, and add controller and routes for fetching storage data for each region

--- server/app/config.js
+++ server/app/config.js
@@ -2,60 +2,74 @@ module.exports = {
   year: 2022,
   regions: {
     CA: {
-      eere: "eere-defaults-california.json",
       rdf: "rdf_CA_2022.json",
+      eere: "eere-defaults-california.json",
+      storage: "storage-defaults-california.json",
     },
     CENT: {
-      eere: "eere-defaults-central.json",
       rdf: "rdf_CENT_2022.json",
+      eere: "eere-defaults-central.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     FL: {
-      eere: "eere-defaults-florida.json",
       rdf: "rdf_FL_2022.json",
+      eere: "eere-defaults-florida.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     MIDA: {
-      eere: "eere-defaults-midatlantic.json",
       rdf: "rdf_MIDA_2022.json",
+      eere: "eere-defaults-midatlantic.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     MIDW: {
-      eere: "eere-defaults-midwest.json",
       rdf: "rdf_MIDW_2022.json",
+      eere: "eere-defaults-midwest.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     NCSC: {
-      eere: "eere-defaults-carolinas.json",
       rdf: "rdf_NCSC_2022.json",
+      eere: "eere-defaults-carolinas.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     NE: {
-      eere: "eere-defaults-newengland.json",
       rdf: "rdf_NE_2022.json",
+      eere: "eere-defaults-newengland.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     NW: {
-      eere: "eere-defaults-northwest.json",
       rdf: "rdf_NW_2022.json",
+      eere: "eere-defaults-northwest.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     NY: {
-      eere: "eere-defaults-newyork.json",
       rdf: "rdf_NY_2022.json",
+      eere: "eere-defaults-newyork.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     RM: {
-      eere: "eere-defaults-rockymountains.json",
       rdf: "rdf_RM_2022.json",
+      eere: "eere-defaults-rockymountains.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     SE: {
-      eere: "eere-defaults-southeast.json",
       rdf: "rdf_SE_2022.json",
+      eere: "eere-defaults-southeast.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     SW: {
-      eere: "eere-defaults-southwest.json",
       rdf: "rdf_SW_2022.json",
+      eere: "eere-defaults-southwest.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     TE: {
-      eere: "eere-defaults-texas.json",
       rdf: "rdf_TE_2022.json",
+      eere: "eere-defaults-texas.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
     TN: {
-      eere: "eere-defaults-tennessee.json",
       rdf: "rdf_TN_2022.json",
+      eere: "eere-defaults-tennessee.json",
+      storage: "storage-defaults-california.json", // TODO: replace with region's actual file
     },
   },
 };
