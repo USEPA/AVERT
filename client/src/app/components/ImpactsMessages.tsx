74daea84a6fd8dc7eeead502cfc48da8feceeb03
Courtney Myers <courtney.myers@erg.com>
2024-03-07 06:26:38 -0500

    Add TODO comment for potential energy storage warning message

--- client/src/app/components/ImpactsMessages.tsx
+++ client/src/app/components/ImpactsMessages.tsx
@@ -225,6 +225,9 @@ function ImpactsMessagesContent() {
     return null;
   }
 
+  // TODO: add warning message indicating there's insufficient solar if the user
+  // enters a small amount of solar and a large amount of battery storage capacity
+
   return (
     <>
       <EquivalentHomesText hourlyChanges={Object.values(totalHourlyChanges)} />
