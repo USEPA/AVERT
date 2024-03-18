57267d0d9da435dee870542f49114996b98ad8ff
Courtney Myers <courtney.myers@erg.com>
2024-03-18 14:20:29 -0400

    Update npm scripts for vite setup and remove old create-react-app config and related files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
 // ---
 import { App } from '@/app/components/App';
 import { store } from '@/app/redux/store';
@@ -15,8 +14,3 @@ render(
   </Provider>,
   container,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- client/src/react-app-env.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react-scripts" />
-/// <reference types="redux-thunk/extend-redux" />
