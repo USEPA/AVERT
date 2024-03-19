fc97130ec830c76af318a61619fa7ff1a196f57e
Courtney Myers <courtney.myers@erg.com>
2024-03-19 14:00:30 -0400

    Rename tailwind-preflight.css to preflight.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 // ---
 import { App } from '@/app/components/App';
 import { store } from '@/app/redux/store';
-import '@/tailwind-preflight.css';
+import '@/preflight.css'; // Tailwind CSS preflight styles, scoped to .twpf
 import '@/styles.css';
 
 const container = document.getElementById('root') as HTMLElement;
