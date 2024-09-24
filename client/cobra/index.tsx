d58b0f7186523dad0b183306d0239a05694ca972
Courtney Myers <courtney.myers@erg.com>
2024-09-24 16:51:39 -0400

    Move (and update path of) style imports in cobra/index.html

--- client/cobra/index.tsx
+++ client/cobra/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "@/styles.css";
 // ---
 import { COBRAPendingMessage } from "../src/components/COBRAPendingMessage";
+import "../src/styles.css";
 
 const container = document.getElementById("avert-cobra") as HTMLElement;
 const root = createRoot(container);
