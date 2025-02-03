21b100253362b1bf6232265ee30d70238fcfeed9
Courtney Myers <courtney.myers@erg.com>
2025-02-03 14:53:16 -0500

    Update ModalDialog component to use updated HeadlessUI transition API

--- client/src/components/ModalDialog.tsx
+++ client/src/components/ModalDialog.tsx
@@ -1,10 +1,5 @@
-import { Fragment, useRef } from "react";
-import {
-  Dialog,
-  DialogPanel,
-  Transition,
-  TransitionChild,
-} from "@headlessui/react";
+import { useRef } from "react";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 // ---
@@ -20,84 +15,71 @@ export function ModalDialog() {
   const cancelRef = useRef<HTMLButtonElement>(null);
 
   return (
-    <Transition show={displayed} as={Fragment}>
-      <Dialog
-        as="div"
-        className={clsx("tw:relative tw:z-10")}
-        initialFocus={cancelRef}
-        open={displayed}
-        onClose={(_ev) => dispatch(resetModalDialog())}
-      >
-        <TransitionChild
-          as={Fragment}
-          enter="tw:duration-300 tw:ease-out"
-          enterFrom="tw:opacity-0"
-          enterTo="tw:opacity-100"
-          leave="tw:duration-200 tw:ease-in"
-          leaveFrom="tw:opacity-100"
-          leaveTo="tw:opacity-0"
-        >
-          <div
-            className={clsx(
-              "tw:fixed tw:inset-0 tw:bg-black/70 tw:transition-colors",
-            )}
-          />
-        </TransitionChild>
+    <Dialog
+      className={clsx("tw:relative tw:z-10")}
+      initialFocus={cancelRef}
+      open={displayed}
+      onClose={(_ev) => dispatch(resetModalDialog())}
+    >
+      <DialogBackdrop
+        transition
+        className={clsx(
+          "tw:fixed tw:inset-0 tw:bg-black/70",
+          "tw:transition tw:!duration-100",
+          "tw:data-closed:opacity-0",
+          "tw:data-enter:ease-out",
+          "tw:data-leave:ease-in",
+        )}
+      />
 
-        <div className={clsx("tw:fixed tw:inset-0 tw:z-10 tw:overflow-y-auto")}>
-          <div
+      <div className={clsx("tw:fixed tw:inset-0 tw:z-10 tw:overflow-y-auto")}>
+        <div
+          className={clsx(
+            "tw:flex tw:min-h-full tw:items-end tw:justify-center tw:p-4",
+            "tw:sm:items-center",
+          )}
+        >
+          <DialogPanel
+            transition
             className={clsx(
-              "tw:flex tw:min-h-full tw:items-end tw:justify-center tw:p-4",
-              "tw:sm:items-center",
+              "tw:relative tw:transform tw:overflow-hidden tw:rounded-lg tw:bg-white tw:p-4 tw:shadow-xl",
+              "tw:transition tw:!duration-100",
+              "tw:sm:w-full tw:sm:max-w-xl tw:sm:p-6",
+              "tw:data-closed:opacity-0 tw:data-closed:sm:translate-y-0",
+              "tw:data-enter:ease-out",
+              "tw:data-leave:ease-in",
             )}
           >
-            <TransitionChild
-              as={Fragment}
-              enter="tw:duration-300 tw:ease-out"
-              enterFrom="tw:translate-y-4 tw:opacity-0 tw:sm:translate-y-0"
-              enterTo="tw:translate-y-0 tw:opacity-100"
-              leave="tw:duration-200 tw:ease-in"
-              leaveFrom="tw:translate-y-0 tw:opacity-100"
-              leaveTo="tw:translate-y-4 tw:opacity-0 tw:sm:translate-y-0"
-            >
-              <DialogPanel
+            <div className="twpf">
+              <div
                 className={clsx(
-                  "tw:relative tw:transform tw:overflow-hidden tw:rounded-lg tw:bg-white tw:p-4 tw:shadow-xl tw:transition-all",
-                  "tw:sm:w-full tw:sm:max-w-xl tw:sm:p-6",
+                  "tw:absolute tw:top-0 tw:right-0 tw:pt-4 tw:pr-4",
                 )}
               >
-                <div className="twpf">
-                  <div
-                    className={clsx(
-                      "tw:absolute tw:top-0 tw:right-0 tw:pt-4 tw:pr-4",
-                    )}
-                  >
-                    <button
-                      type="button"
-                      className={clsx(
-                        "tw:rounded-md tw:bg-white tw:text-gray-400 tw:transition-none",
-                        "tw:hover:text-gray-700",
-                        "tw:focus:text-gray-700",
-                      )}
-                      onClick={(_ev) => dispatch(resetModalDialog())}
-                    >
-                      <span className={clsx("tw:sr-only")}>Close</span>
-                      <XMarkIcon
-                        className={clsx("tw:size-6 tw:transition-none")}
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </div>
+                <button
+                  type="button"
+                  className={clsx(
+                    "tw:rounded-md tw:bg-white tw:text-gray-400 tw:transition-none",
+                    "tw:hover:text-gray-700",
+                    "tw:focus:text-gray-700",
+                  )}
+                  onClick={(_ev) => dispatch(resetModalDialog())}
+                >
+                  <span className={clsx("tw:sr-only")}>Close</span>
+                  <XMarkIcon
+                    className={clsx("tw:size-6 tw:transition-none")}
+                    aria-hidden="true"
+                  />
+                </button>
+              </div>
+            </div>
 
-                <div className={clsx("tw:m-4")}>
-                  <div className="usa-prose">{description}</div>
-                </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
+            <div className={clsx("tw:m-4")}>
+              <div className="usa-prose">{description}</div>
+            </div>
+          </DialogPanel>
         </div>
-      </Dialog>
-    </Transition>
+      </div>
+    </Dialog>
   );
 }
